076486ad26a6757b778c9f1dda3659c38dceff98
Aidan Feldman <aidan.feldman@gsa.gov>
2016-12-30 00:23:02 -0500

    accept alternates for credential field pairs

--- models/credentials.go
+++ models/credentials.go
@@ -8,17 +8,45 @@ type ServiceKeyResponse struct {
 
 type ServiceKeyResource struct {
 	Entity struct {
-		Credentials Credentials `json:"credentials"`
+		Credentials CredentialsJSON `json:"credentials"`
 	} `json:"entity"`
 }
 
-type Credentials struct {
-	DBName   string `json:"db_name"`
-	Name     string `json:"name"`
+// http://stackoverflow.com/a/28035946/358804
+type CredentialsJSON struct {
+	// these groups of fields should be interchangeable
+	DBName string `json:"db_name"`
+	Name   string `json:"name"`
+
 	Host     string `json:"host"`
+	Hostname string `json:"hostname"`
+	///////////////////////////////////////////////////
+
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Port     string `json:"port"`
+}
+
+func (c *CredentialsJSON) GetDBName() string {
+	if c.Name != "" {
+		return c.Name
+	}
+	return c.DBName
+}
+
+func (c *CredentialsJSON) GetHost() string {
+	if c.Host != "" {
+		return c.Host
+	}
+	return c.Hostname
+}
+
+type Credentials struct {
+	DBName   string
+	Host     string
+	Username string
+	Password string
+	Port     string
 }
 
 func CredentialsFromJSON(body string) (creds Credentials, err error) {
@@ -27,6 +55,15 @@ func CredentialsFromJSON(body string) (creds Credentials, err error) {
 	if err != nil {
 		return
 	}
-	creds = serviceKeyResponse.Resources[0].Entity.Credentials
+	jsonCreds := serviceKeyResponse.Resources[0].Entity.Credentials
+
+	creds = Credentials{
+		DBName:   jsonCreds.GetDBName(),
+		Host:     jsonCreds.GetHost(),
+		Username: jsonCreds.Username,
+		Password: jsonCreds.Password,
+		Port:     jsonCreds.Port,
+	}
+
 	return
 }
---
+++ models/credentials_test.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+)
+
+const JSONWrap = `{
+	"resources": [
+		{
+			"entity": {
+				"credentials": %s
+			}
+		}
+	]
+}`
+
+type credentialsFromJSONTest struct {
+	entityJSON     string
+	expectedDBName string
+	expectedHost   string
+}
+
+func TestCredentialsFromJSON(t *testing.T) {
+	tests := []credentialsFromJSONTest{
+		{
+			`{
+				"db_name": "name",
+				"host": "host.com",
+				"password": "pass",
+				"port": "5432",
+				"username": "user"
+			}`,
+			"name",
+			"host.com",
+		},
+		{
+			`{
+				"name": "name",
+				"hostname": "host.com",
+				"password": "pass",
+				"port": "5432",
+				"username": "user"
+			}`,
+			"name",
+			"host.com",
+		},
+	}
+
+	for _, test := range tests {
+		fullJSON := fmt.Sprintf(JSONWrap, test.entityJSON)
+		creds, err := CredentialsFromJSON(fullJSON)
+		if err != nil {
+			t.Error(err)
+		}
+		if creds.Host != test.expectedHost {
+			t.Errorf("Expected: %v. Actual: %v.", test.expectedHost, creds.Host)
+		}
+		if creds.DBName != test.expectedDBName {
+			t.Errorf("Expected: %v. Actual: %v.", test.expectedDBName, creds.DBName)
+		}
+	}
+}
