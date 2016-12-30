131f870b8709cc680f8153aabba8613c52edafaa
Aidan Feldman <aidan.feldman@gsa.gov>
2016-12-30 00:27:31 -0500

    remove boilerplate comments from main.go

--- main.go
+++ main.go
@@ -16,18 +16,11 @@ const SUBCOMMAND = "connect-to-db"
 // be found at  "code.cloudfoundry.org/cli/plugin/plugin.go"
 type DBConnectPlugin struct{}
 
-// Run must be implemented by any plugin because it is part of the
-// plugin interface defined by the core CLI.
-//
-// Run(....) is the entry point when the core CLI is invoking a command defined
-// by a plugin. The first parameter, plugin.CliConnection, is a struct that can
+// Run is the entry point when the core CLI is invoking a command defined
+// by the plugin. The first parameter, plugin.CliConnection, is a struct that can
 // be used to invoke cli commands. The second paramter, args, is a slice of
 // strings. args[0] will be the name of the command, and will be followed by
 // any additional arguments a cli user typed in.
-//
-// Any error handling should be handled with the plugin itself (this means printing
-// user facing errors). The CLI will exit 0 if the plugin exits 0 and will exit
-// 1 should the plugin exits nonzero.
 func (c *DBConnectPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	// check to ensure it's the right subcommand, not others like CLI-MESSAGE-UNINSTALL
 	if args[0] != SUBCOMMAND {
@@ -49,18 +42,6 @@ func (c *DBConnectPlugin) Run(cliConnection plugin.CliConnection, args []string)
 	}
 }
 
-// GetMetadata must be implemented as part of the plugin interface
-// defined by the core CLI.
-//
-// GetMetadata() returns a PluginMetadata struct. The first field, Name,
-// determines the name of the plugin which should generally be without spaces.
-// If there are spaces in the name a user will need to properly quote the name
-// during uninstall otherwise the name will be treated as seperate arguments.
-// The second value is a slice of Command structs. Our slice only contains one
-// Command Struct, but could contain any number of them. The first field Name
-// defines the command `cf basic-plugin-command` once installed into the CLI. The
-// second field, HelpText, is used by the core CLI to display help information
-// to the user in the core commands `cf help`, `cf`, or `cf -h`.
 func (c *DBConnectPlugin) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
 		Name: "DBConnect",
@@ -78,9 +59,6 @@ func (c *DBConnectPlugin) GetMetadata() plugin.PluginMetadata {
 			{
 				Name:     SUBCOMMAND,
 				HelpText: "Basic plugin command's help text",
-
-				// UsageDetails is optional
-				// It is used to show help of usage of each command
 				UsageDetails: plugin.Usage{
 					Usage: SUBCOMMAND + "\n   cf " + SUBCOMMAND + " <app_name> <service_instance_name>",
 				},
@@ -89,20 +67,6 @@ func (c *DBConnectPlugin) GetMetadata() plugin.PluginMetadata {
 	}
 }
 
-// Unlike most Go programs, the `Main()` function will not be used to run all of the
-// commands provided in your plugin. Main will be used to initialize the plugin
-// process, as well as any dependencies you might require for your
-// plugin.
 func main() {
-	// Any initialization for your plugin can be handled here
-	//
-	// Note: to run the plugin.Start method, we pass in a pointer to the struct
-	// implementing the interface defined at "code.cloudfoundry.org/cli/plugin/plugin.go"
-	//
-	// Note: The plugin's main() method is invoked at install time to collect
-	// metadata. The plugin will exit 0 and the Run([]string) method will not be
-	// invoked.
 	plugin.Start(new(DBConnectPlugin))
-	// Plugin code should be written in the Run([]string) method,
-	// ensuring the plugin environment is bootstrapped.
 }
