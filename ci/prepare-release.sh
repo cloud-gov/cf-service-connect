cf2afbec4266ce2db410a778898f413612d05cc5
Mark Boyd <mark.boyd@gsa.gov>
2023-08-24 14:14:39 -0400

    enable step to create release on GitHub from pipeline

--- ci/prepare-release.sh
+++ ci/prepare-release.sh
@@ -3,8 +3,5 @@
 TAG=$(git describe --tags)
 echo "$TAG" > tag
 
-LAST_TAG=$(git describe --tags --abbrev=0)
-git log "$LAST_TAG..HEAD" --oneline --no-decorate > releaselog.md
-
 # Create release binaries
 ./bin/create-release-binaries.sh
